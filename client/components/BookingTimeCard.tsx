9974c8812fb444bdcd5743c019f5f2ade03d5687
Frasier <fsundra@gmail.com>
2025-12-06 12:05:43 +0800

    Adds simple logic for time generation - still needs backend generatoin

--- client/components/BookingTimeCard.tsx
+++ client/components/BookingTimeCard.tsx
@@ -1,13 +1,28 @@
-"use client"
-
+"use client";
 
 const BookingTimeComponent = () => {
-    return (
-        <div>
-            BookingTimeComponent
-        </div>
-    )
+    const timeConstruction = () => {
+        // Note: this needs to take in a start_time and end_time (when it is available)
+        const times = [];
+        for (let i = 0; i < 24; i++) {
+            const hour = i.toString();
+            const minutes = i % 2 == 0 ? ":00" : ":30";
+            const total_time = hour + minutes;
 
-}
+            times.push(<button className="p-2 rounded-xl border border-slate-200 hover:-translate-y-0.5 hover:bg-slate-100 transition-all duration-150 ease-in-out"> {total_time} </button>);
+        }
+        return times;
+    };
+    return (
+        <div className="w-full bg-white rounded-xl border-slate-200 shadow-xl p-8 space-y-4">
+        <h5 className="text-lg font-bold">
+            Select a Time
+        </h5>
+            <div className="grid grid-cols-3 gap-4">
+                {timeConstruction()}
+            </div>
+        </div>
+    );
+};
 
 export default BookingTimeComponent;
