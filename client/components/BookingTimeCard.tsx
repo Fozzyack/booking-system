fffa0d0ce0412b0ea132d94529cb09b91cfa1011
Frasier <fsundra@gmail.com>
2025-12-10 21:42:55 +0800

    Styling (fits on mobile

--- client/components/BookingTimeCard.tsx
+++ client/components/BookingTimeCard.tsx
@@ -27,7 +27,7 @@ const BookingTimeComponent: React.FC<BookingTimeComponentType> = ({
                 <button
                     key={i}
                     onClick={() => setSelectedTime(total_time)}
-                    className={`py-2 px-1 text-sm rounded-lg border border-slate-200 hover:-translate-y-0.5 transition-all duration-150 ease-in-out  ${selectedTime == total_time ? "bg-primary text-white hover:bg-primary/80" : "bg-white hover:bg-slate-100"} transition-all ease-in-out duration-300`}
+                    className={`py-1 px-1 text-sm rounded-lg border border-slate-200 hover:-translate-y-0.5 transition-all duration-150 ease-in-out  ${selectedTime == total_time ? "bg-primary text-white hover:bg-primary/80" : "bg-white hover:bg-slate-100"} transition-all ease-in-out duration-300`}
                 >
                     {total_time}
                 </button>,
@@ -38,7 +38,7 @@ const BookingTimeComponent: React.FC<BookingTimeComponentType> = ({
     return (
         <div className="w-full bg-white rounded-xl border-slate-200 shadow-xl p-8 space-y-4">
             <h5 className="text-lg font-bold">Select a Time</h5>
-            <div className="grid grid-cols-4 gap-2">{timeConstruction()}</div>
+            <div className="grid grid-cols-4 gap-1">{timeConstruction()}</div>
             <button className="flex-1 w-full px-4 py-2 border border-primary rounded-lg hover:bg-gray-50 hover:-translate-y-1 transition-all ease-in-out">
                 Clear
             </button>
